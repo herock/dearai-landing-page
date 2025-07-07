02892bfbeff204028fe47c3904a2638045bb29d4
herock <herock@gmail.com>
2025-07-07 21:16:47 +0800

    update

--- functions/alex/[[path]].js
+++ functions/alex/[[path]].js
@@ -1,36 +1,34 @@
 export async function onRequest(context) {
-    // 从上下文中获取 R2 存储桶的绑定
-    const { env, params, next } = context;
+    const { env, params, request, next } = context;
     const bucket = env.ARTICLES_BUCKET;
   
-    // 1. 构造 R2 中的对象键 (Object Key)
-    // params.path 是一个数组，包含了 /alex/ 后面的所有路径部分
-    // 例如，访问 /alex/index.html, params.path 就是 ['index.html']
-    // 访问 /alex/letters/some-article.html, params.path 就是 ['letters', 'some-article.html']
-    let key = 'alex/' + params.path.join('/');
-  
-    // 2. 处理对目录的请求 (例如 /alex/ 或 /alex/letters/)
-    // 如果请求的路径以 / 结尾，或者不包含点 (很可能是一个目录)，
-    // 我们就默认去查找该目录下的 index.html 文件。
-    if (key.endsWith('/') || !key.includes('.')) {
-      if (!key.endsWith('/')) {
-        key += '/';
-      }
-      key += 'index.html';
-    }
-  
-    // 3. 从 R2 获取对象
-    const object = await bucket.get(key);
-  
-    // 4. 处理未找到对象的情况
-    if (object === null) {
-      // 在 R2 中没有找到对应的文件，将请求传递给静态资源处理器
-      // 这允许 Pages 仍然可以提供 /css/style.css 等其他静态文件
+    // 1. 检查请求是否来自我们的 Function 自身，避免无限循环
+    // 这是个安全措施，防止 next() 调用后又回到这里
+    if (request.headers.get('x-pages-function-invoked')) {
       return next();
     }
   
-    // 5. 成功找到对象，返回响应
-    // 从对象中提取 HTTP 元数据 (如 Content-Type)
+    // 2. 构造 R2 中的对象键 (Object Key)
+    let key = 'alex/' + params.path.join('/');
+  
+    // 3. 处理对目录的请求，默认查找 index.html
+    if (key.endsWith('/')) {
+      key += 'index.html';
+    }
+  
+    // 4. 从 R2 获取对象
+    const object = await bucket.get(key);
+  
+    // 5. 如果在 R2 中没有找到对象，把请求交给 Pages 的静态处理器
+    if (object === null) {
+      // 设置一个请求头，标记我们已经尝试过 Function 处理
+      const newRequest = new Request(request);
+      newRequest.headers.set('x-pages-function-invoked', 'true');
+      // 把带有标记的新请求交给 next()
+      return next(newRequest);
+    }
+  
+    // 6. 成功找到对象，返回响应
     const headers = new Headers();
     object.writeHttpMetadata(headers);
     headers.set('etag', object.httpEtag);
